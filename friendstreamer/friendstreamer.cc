63942ae2d4798c734f9dd9fb9e56228db7e61a3d
shiiion <mayansymbol@gmail.com>
2022-04-21 19:36:55 -0700

    Windows builds

--- friendstreamer/friendstreamer.cc
+++ friendstreamer/friendstreamer.cc
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <mutex>
+#include <memory>
 #include <thread>
 #include <vector>
 
@@ -11,54 +12,37 @@
 
 constexpr asio::ip::port_type kPortNum = 40040;
 
-enum class PacketType : uint8_t {
-    UNKNOWN,
-    DATA,
-    CONTROL
-};
-
-enum class ControlMessageType : uint8_t {
-    SKIP,
-    PAUSE,
-    PLAY
-};
-
-struct packet {
-    PacketType packet_type;
-    uint32_t len;
-};
-
-struct data_packet : packet {
-    uint32_t offset;
-    uint32_t size;
-    void *data;
-};
-
-struct control_packet : packet {
-    ControlMessageType control_type;
-    uint32_t skip_pos;
-};
-
 struct asio_socket { 
-    asio_socket() : skt(io_svc) {}
-    asio_socket(asio_socket&&) = delete;
+    asio_socket(std::shared_ptr<asio::io_service> io_svc) : io_svc(io_svc), skt(*this->io_svc.get()) {}
+    asio_socket(asio_socket&& rhs) : io_svc(std::move(rhs.io_svc)), skt(std::move(rhs.skt)) {}
     asio_socket(asio_socket const&) = delete;
 
-    asio::io_service io_svc;
+    asio_socket& operator=(asio_socket&& rhs) {
+        io_svc = std::move(rhs.io_svc);
+        skt = std::move(rhs.skt);
+        return *this;
+    }
+    asio_socket& operator=(asio_socket const&) = delete;
+
+    ~asio_socket() {
+        skt.close();
+    }
+
+    std::shared_ptr<asio::io_service> io_svc;
     asio::ip::tcp::socket skt;
 };
 
 struct host_data {
     std::vector<asio_socket> clients;
-    std::thread* acceptor_thread;
-    std::thread* network_thread;
+    std::unique_ptr<std::thread> acceptor_thread;
+    std::unique_ptr<std::thread> network_thread;
     std::ifstream file_handle;
     std::mutex data_m;
 };
 
 struct client_data {
-    asio::ip::tcp::socket host;
-    std::thread* network_thread;
+    std::unique_ptr<asio::ip::tcp::socket> host;
+    std::unique_ptr<std::thread> network_thread;
     std::fstream buffer_handle;
 };
 
@@ -66,19 +50,18 @@ void client_network_thread(client_data* data) {
     std::string recv_buffer;
     recv_buffer.resize(1500);
     asio::error_code ec;
-    packet pkt = {PacketType::CONTROL, 0};
     while (true) {
-        size_t recv_size = data->host.receive(asio::buffer(recv_buffer), 0, ec);
+        size_t recv_size = data->host->receive(asio::buffer(recv_buffer), 0, ec);
         if (recv_size == 0 || ec.value() != 0) {
             break;
         }
         char const* data = recv_buffer.data();
-        if (pkt.packet_type == PacketType::UNKNOWN) {
-            auto recv_pkt = reinterpret_cast<packet const*>(data);
-            pkt.packet_type = recv_pkt->packet_type;
-            pkt.len = recv_pkt->len;
-            data += sizeof(packet);
-        }
+        // if (pkt.packet_type == PacketType::UNKNOWN) {
+        //     auto recv_pkt = reinterpret_cast<packet const*>(data);
+        //     pkt.packet_type = recv_pkt->packet_type;
+        //     pkt.len = recv_pkt->len;
+        //     data += sizeof(packet);
+        // }
     }
 }
 
@@ -97,9 +80,10 @@ void host_network_thread(host_data* data) {
 }
 
 void host_acceptor_thread(host_data* data) {
+    auto io_svc = std::make_shared<asio::io_service>();
     while (true) {
-        asio_socket new_conn;
-        asio::ip::tcp::acceptor acceptor(new_conn.io_svc, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), kPortNum));
+        asio_socket new_conn(io_svc);
+        asio::ip::tcp::acceptor acceptor(*io_svc, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), kPortNum));
         asio::socket_base::reuse_address reuse(true);
         acceptor.set_option(reuse);
         acceptor.accept(new_conn.skt);
@@ -115,19 +99,19 @@ void host_acceptor_thread(host_data* data) {
 
 extern "C" {
 int fs_fill_buffer(struct fs_data *s, void *buffer, int max_len) {
-    
+    return 0;
 }
 
 int fs_seek(struct fs_data *s, int64_t pos) {
-
+    return 0;
 }
 
 int64_t fs_get_size(struct fs_data *s) {
-
+    return 0;
 }
 
 void fs_close(struct fs_data *s) {
-
+    return;
 }
 
 void open_stream(struct fs_data *s, bool is_host, char const* url) {
@@ -136,14 +120,14 @@ void open_stream(struct fs_data *s, bool is_host, char const* url) {
         host_data* new_host_data = new host_data;
         new_host_data->file_handle = std::ifstream(url);
         s->host_private = new_host_data;
-        new_host_data->acceptor_thread = new std::thread(host_acceptor_thread, s);
-        new_host_data->network_thread = new std::thread(host_network_thread, s);
+        new_host_data->acceptor_thread = std::make_unique<std::thread>(host_acceptor_thread, new_host_data);
+        new_host_data->network_thread = std::make_unique<std::thread>(host_network_thread, new_host_data);
     } else {
         // TODO: Decode URL to determine host
         client_data* new_client_data = new client_data;
         new_client_data->buffer_handle = std::fstream(".streambuf", std::ios::trunc | std::ios::binary | std::ios::in | std::ios::out);
         s->client_private = new_client_data;
-        new_client_data->network_thread = new std::thread(client_network_thread, s);
+        new_client_data->network_thread = std::make_unique<std::thread>(client_network_thread, new_client_data);
     }
 }
 
